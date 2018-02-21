ecfcf1d39a9d20b414193005138be8cdf008c939
Michael Brinkmann <michael.brinkmann@wallmedien.de>
2018-02-21 12:24:05 +0100

    Projektdateien hinzufügen.

---
+++ SampleDynamicDllWithRoslyn/CompileAssembly.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.CodeDom.Providers.DotNetCompilerPlatform;
+using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace SampleDynamicDllWithRoslyn {
+
+	public class CompileAssembly {
+
+		public static void Main(string[] args) {
+
+			// source to be compiled
+			string classSource = @"using System;
+namespace DynamicDll {
+	public class SomeClass {
+		public void SomeMethod() {
+			// some source
+			int y = 0;
+			y++;
+			if (y > 0) {
+				Console.WriteLine(y);
+			}
+		}
+	}
+}";
+
+			CompilerParameters options = new CompilerParameters {
+				GenerateExecutable = false,
+				GenerateInMemory = true,
+				IncludeDebugInformation = true,
+				CompilerOptions = "/define:DEBUG"
+			};
+
+			CodeDomProvider provider = new CSharpCodeProvider();
+			//FixCompilerPath(provider);
+
+			// The following line throws Exception, because compiler is copied to D:\bin\roslyn
+			//
+			// Ausnahme ausgelöst: "System.IO.DirectoryNotFoundException" in Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll
+			// Ein Ausnahmefehler des Typs "System.IO.DirectoryNotFoundException" ist in Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll aufgetreten.
+			// Ein Teil des Pfades "D:\Projects\SampleDynamicDllWithRoslyn\SampleDynamicDllWithRoslyn\bin\Debug\bin\roslyn\csc.exe" konnte nicht gefunden werden.
+			CompilerResults result = provider.CompileAssemblyFromSource(options, classSource);
+
+			// Keep console open
+			Console.ReadLine();
+		}
+
+		/// <summary>
+		/// Fixes the compiler path to get it run.
+		/// </summary>
+		private static void FixCompilerPath(CodeDomProvider provider) {
+			FieldInfo memberInfo = provider.GetType().GetField("_compilerSettings", BindingFlags.Instance | BindingFlags.NonPublic);
+			if (memberInfo != null) {
+				object settings = memberInfo.GetValue(provider);
+				FieldInfo path = settings.GetType().GetField("_compilerFullPath", BindingFlags.Instance | BindingFlags.NonPublic);
+				path?.SetValue(settings, @"D:\bin\roslyn\csc.exe");
+			}
+		}
+
+	}
+}
---
+++ SampleDynamicDllWithRoslyn/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("SampleDynamicDllWithRoslyn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleDynamicDllWithRoslyn")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("d0014ad1-8d90-45ca-904f-266285846c81")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
